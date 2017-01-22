a8cddd6f31e52c97905b1e664517f6e8397bbc25
Tuxos <dennis@deitermann.org>
2017-01-22 17:53:48 +0100

    Update

--- Sonos-HM-Remote/module.php
+++ Sonos-HM-Remote/module.php
@@ -6,9 +6,9 @@
 
 		parent::Create();
 
-		$this->RegisterPropertyInteger("iddisplay", "");
 		$this->RegisterPropertyInteger("idsonos", "");
 		$this->RegisterPropertyString("serial6t", "");
+		$this->RegisterPropertyString("iddisplay", "");
 		$this->RegisterPropertyString("zeile1", "Temperatur");
 		$this->RegisterPropertyString("zeile2", "Garten");
 		$this->RegisterPropertyInteger("idvar", "");
@@ -28,14 +28,10 @@
 
 	}
 
-	// Berechne Kosten
-	public function price() {
+	// Zeige Sonos ID
+	public function showid() {
 
-		$meteryesterday = GetValue(IPS_GetObjectIDByName("Verbrauch gestern",$this->InstanceID));
-
-		SetValue(IPS_GetObjectIDByName("Kosten gestern",$this->InstanceID), ($meteryesterday*$this->ReadPropertyFloat("price"))/100);
-
-
+		echo $this->ReadPropertyString('idsonos');
 
 
 		}
